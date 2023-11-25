137e886277c195a867334138eba80dcf261e12fc
Tareq905 <tareqshah.027@gmail.com>
2023-11-25 07:27:02 +0600

    first commit

---
+++ database/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb://127.0.0.1:27017/task-manager';
+const options = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+};
+
+mongoose
+	.connect(mongoURI, options)
+	.then(() => {
+		console.log('Connected to MongoDB');
+	})
+	.catch((error) => {
+		console.error('Error connecting to MongoDB:', error);
+	});
